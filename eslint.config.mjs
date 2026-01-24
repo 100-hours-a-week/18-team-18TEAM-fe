9a3018ff6f443957a95b72307ad37936bf6a275a
yeiin <88276674+yeiin@users.noreply.github.com>
2026-01-24 18:45:49 +0900

    fix: eslint.config 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,26 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
+const eslintConfig = defineConfig([
+  
+  ...nextVitals,
+  ...nextTs,
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    ignores: [
+    ...globalIgnores([
       'node_modules/**',
       '.next/**',
       'out/**',
       'build/**',
       'next-env.d.ts',
-    ],
+    ]),
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-]
+])
 
 export default eslintConfig
