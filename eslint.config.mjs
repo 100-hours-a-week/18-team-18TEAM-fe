3dbf843363f66d8323a93155981fafd42c319af9
yeiin <88276674+yeiin@users.noreply.github.com>
2026-01-24 19:01:02 +0900

    fix: eslint 버전에 따른 flat config false 처리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,29 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals.js'
-import nextTs from 'eslint-config-next/typescript.js'
+// import { dirname } from 'path'
+// import { fileURLToPath } from 'url'
+// import { FlatCompat } from '@eslint/eslintrc'
 
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
 
-const eslintConfig = defineConfig([
-  
-  ...nextVitals,
-  ...nextTs,
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// })
 
-  {
-    ...globalIgnores([
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ]),
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
-])
+// const eslintConfig = [
+//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+//   {
+//     ignores: [
+//       'node_modules/**',
+//       '.next/**',
+//       'out/**',
+//       'build/**',
+//       'next-env.d.ts',
+//     ],
+//     rules: {
+//       '@typescript-eslint/no-unused-vars': 'warn',
+//       '@typescript-eslint/no-explicit-any': 'off',
+//     },
+//   },
+// ]
 
-export default eslintConfig
+// export default eslintConfig
