325f700439bee73a84c58332369d6afcf05d485f
yunyoungyee <eric3725@naver.com>
2026-03-07 18:59:11 +0900

    chore: 코드정리

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -7,7 +7,8 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ?? process.env.NODE_ENV,
+  environment:
+    process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ?? process.env.NODE_ENV,
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
